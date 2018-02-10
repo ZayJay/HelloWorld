3494a14f65a6745a608c969d07e89dec318cbe0d
Steven Zajac <Zajac.Steven@gmail.com>
2018-02-09 22:14:07 -0500

    Commit initial project files.
    Get HelloWorld files into git to test files necessary for getting
    project under source control.

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,25 @@
+/*
+ * File: HelloWorld.java
+ * Author: Steven Zajac
+ * Created Date: 2018-01-30
+ *
+ * Super-simple hello world!
+ *
+ */
+package helloworld;
+
+/**
+ *
+ * @author szajac
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World!");
+    }
+    
+}
